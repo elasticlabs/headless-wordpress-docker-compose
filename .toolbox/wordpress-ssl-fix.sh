196cb619c1edd85b30181ae1fb4917ef1b918caf
elasticlabs <patrice.mauny@gmail.com>
2020-12-21 23:35:28 +0100

    Fixes

--- .toolbox/wordpress-ssl-fix.sh
+++ .toolbox/wordpress-ssl-fix.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #
-# This scripts fixes an issue with WP CLI which introduces a SSL mess while setting up wordpress
+# This script fixes an issue with WP CLI which introduces a SSL mess while setting up wordpress
 
-/scripts/message.sh warning "⚙️ [UNSTABLE] Fix WP CLI messing with SSL management in wp-config-php..."
+/scripts/message.sh warning "...... [UNSTABLE] Fix WP CLI messing with SSL management in wp-config-php..."
 
 # Keep wp-config.php config
 sed -i '1,/table_prefix/!d ' wp-config.php
 
 # Add end SSL specific configs from wp-config.old.php
-number=$(shell sed -n  '\|table_prefix|=' wp-config.old.php)
-lines=$(shell wc -l wp-config.old.php)
-sed '/table_prefix/,\${lines}!d ' wp-config.old.php | grep -v table_prefix >> wp-config.php
+number=$(sed -n  '\|table_prefix|=' wp-config.old.php)
+sed '/table_prefix/,$!d ' wp-config.old.php | grep -v table_prefix >> wp-config.php
